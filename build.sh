e560eccc4a4d0fe6617474b4f5dd7f8e37c4b3ef
c3cc3son <gwisang.choi@gmail.com>
2024-02-01 01:31:13 +0900

    change build.sh by notebook

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+set -x
 javac -classpath ${FQ_LIB_HOME}/FileQueueJNI.jar TestDeQXA.java
 javac -classpath ${FQ_LIB_HOME}/FileQueueJNI.jar TestEnQ_loop.java
 javac -classpath ${FQ_LIB_HOME}/FileQueueJNI.jar:.:json-20210307.jar AgentJson_enQ.java
