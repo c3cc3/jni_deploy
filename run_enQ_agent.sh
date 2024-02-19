97c88d8ac2697e672052e4c6b0206171ec30eb63
c3cc3 <gwisang.choi@gmail.com>
2024-02-20 03:21:21 +0900

    change run_enQ_agent.sh run_deQ_agent.sh

--- run_enQ_agent.sh
+++ run_enQ_agent.sh
@@ -1,1 +1,2 @@
-java -cp .:FileQueueJNI.jar:.:json-20210307.jar AgentJson_enQ 172.30.1.87 7777
+#java -cp .:FileQueueJNI.jar:.:json-20210307.jar AgentJson_enQ 172.30.1.87 7777
+java -cp /home/ums/work/jni_deploy/lib/FileQueueJNI.jar:.:/home/ums/work/jni_deploy/lib/json-20210307.jar AgentJson_enQ 172.30.1.87 7777
